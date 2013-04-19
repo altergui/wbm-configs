3ba8663ec3521745421741b1ec2dbb65503b1848
Gui Iribarren <gui@altermundi.net>
2013-04-19 14:35:57 +0200

    hacky bat-hosts maker

---
+++ update-bat-hosts.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+calcMAC () { echo "obase=16;ibase=16;$*"| tr -d : | bc | sed "s/\(..\)/\1:/g;s/:$//" ; }
+
+echo "### Any changes past this line will get overwritten ###" >> bat-hosts
+sed -i "/### Any changes past this line will get overwritten ###/q" bat-hosts
+( for file in */.mac ; do
+  MACADDR="$(cat $file | tr abcdef ABCDEF)"
+  export IFADDR="
+$(calcMAC "$MACADDR +00:00:00:00:00:00") HOSTNAME_2
+$(calcMAC "$MACADDR +00:00:0A:00:00:01") HOSTNAME_5"
+  echo "$IFADDR" | sed "s/HOSTNAME/$(uci -c ${file%.mac}/overlay/etc/config/ get system.@system[0].hostname)/"
+done ) >> bat-hosts
+
+
